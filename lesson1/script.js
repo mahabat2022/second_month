482de14b737d0482755d59aff7b2efedc3f0ab14
mahabat2022 <kebekbaevamahabat5@gmail.com>
2022-05-07 19:57:43 +0300

    lesson1

---
+++ lesson1/script.js
@@ -0,0 +1,71 @@
+// const  a = "a" ;// неизменяемая
+//
+// console .log(a);
+//
+// let  c = "32"; //namber
+//
+// const bool = true ; //boolean
+// const bool2 = false;
+//
+// console.log(typeof c)
+//
+// const obj = {
+//     key:"value",
+//     username: "Sara",
+//     data: {
+//         first_name:"Jace",
+//         Last_name:"rus",
+//     }
+// }
+//
+//
+// console.log(obj);
+// console.log(obj.data.first_name);
+//
+// const arr =["aigulia", true, false, 34]
+// console.log(arr[1],arr[0],arr[2],arr[3]);
+//
+//
+//
+// const arr2 = [
+//     {
+//         key: 'value'
+//     },  {
+//         key: 'value'
+//     },  {
+//         key: 'value2'
+//     },
+// ]
+// console.log(arr2[2].key);
+//
+// const name = prompt(massege:"как вас зовут?")
+// console.log("Hello" + name)
+//
+//
+// const age = alert("вам еще нет восемнадцати")
+//
+//
+// const first_number = prompt("Первае число");
+// const first_number = prompt("Второре число");
+//
+if (first_number > last_number){
+    console.log(first_number + ">" + last_number)
+
+}
+
+
+
+const age = prompt("Сколько вам лет?")
+if(age > 18){
+    console.log("Вам нельзя входить")
+}else if(age === 18){
+    console.log("Вам можно входить")
+
+} else {
+    console.log("Нельзя входить")
+}
+
+console.log(30 * 40 - 203)
+console.log(-30 * 203)
+console.log(3**4)
+console.log(3* 20 / 11)
